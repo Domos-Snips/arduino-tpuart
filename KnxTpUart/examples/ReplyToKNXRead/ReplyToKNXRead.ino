4ba487ba4aff1c7ff8fedfae82ff10313ed23d05
Thorsten Gehrig <thorsten@gehrig.info>
2015-11-19 19:05:52 +0000

    Adapted to new structure

--- KnxTpUart/examples/ReplyToKNXRead/ReplyToKNXRead.ino
+++ KnxTpUart/examples/ReplyToKNXRead/ReplyToKNXRead.ino
@@ -1,14 +1,11 @@
 #include <KnxTpUart.h>
 
-// Define group address to react on
-String my_address = "0/0/100";
-
 // Initialize the KNX TP-UART library on the Serial1 port of Arduino Mega
-KnxTpUart knx(&Serial1, "15.15.20");
+KnxTpUart knx(&Serial1, "1.1.15");
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("TP-UART Test");  
+  Serial.println("TP-UART Test");
 
   Serial1.begin(19200);
   UCSR1C = UCSR1C | B00100000; // Even Parity
@@ -23,7 +20,8 @@ void setup() {
   Serial.println(UCSR1C, BIN);
 
   knx.uartReset();
-  knx.addListenGroupAddress(my_address);
+  // Define group address to react on
+  knx.addListenGroupAddress("3/0/100);
 }
 
 
@@ -33,19 +31,18 @@ void loop() {
 void serialEvent1() {
   KnxTpUartSerialEventType eType = knx.serialEvent();
   if (eType == KNX_TELEGRAM) {
-     KnxTelegram* telegram = knx.getReceivedTelegram();
+    KnxTelegram* telegram = knx.getReceivedTelegram();
 
-     // Is it a read request?
-     if (telegram->getCommand() == KNX_COMMAND_READ) {
-        //knx.groupAnswerBool(my_address, true);
-        knx.groupAnswer2ByteFloat(my_address, 25.28);
-     }   
+    // Telegrammauswertung auf KNX (bei Empfang immer notwendig)
+    String target =
+      String(0 + telegram->getTargetMainGroup())   + "/" +
+      String(0 + telegram->getTargetMiddleGroup()) + "/" +
+      String(0 + telegram->getTargetSubGroup());
+
+    // Is it a read request?
+    if (telegram->getCommand() == KNX_COMMAND_READ) {
+      //knx.groupAnswerBool("3/0/100", true);
+      knx.groupAnswer2ByteFloat("3/0/100", 25.28);
+    }
   }
 }
-
-
-
-
-
-
-
