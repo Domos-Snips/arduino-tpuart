e9ab5cc084e99fde818122cc48c8564282187d3b
Thorsten Gehrig <thorsten@gehrig.info>
2015-11-21 15:52:09 +0000

    Groupread.ino wurden online mit Bitbucket bearbeitet

--- KnxTpUart/examples/GroupRead/Groupread.ino
+++ KnxTpUart/examples/GroupRead/Groupread.ino
@@ -1,1 +1,17 @@
-test
+#include <KnxTpUart.h>
+
+KnxTpUart knx(&Serial, "1.1.199");
+
+void setup() {
+  Serial.begin(19200);
+  UCSR0C = UCSR0C | B00100000;
+  knx.uartReset();
+}
+
+void loop() {
+   knx.groupReadBool("2/6/0");  // Zum Senden einer Leseanfrage vom Arduino zum KNX auf der Gruppenadresse "2/6/0"
+}
+
+void serialEvent() {
+  KnxTpUartSerialEventType eType = knx.serialEvent();
+}
