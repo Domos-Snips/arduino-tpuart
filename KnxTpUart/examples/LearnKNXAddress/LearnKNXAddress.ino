a2d02b2c4ce72204ee2004f6a883228f0a214f83
Mag Gyver <foxfox@versanet.de>
2016-03-01 17:44:35 +0000

    LearnKNXAddress.ino wurden online mit Bitbucket bearbeitet

--- KnxTpUart/examples/LearnKNXAddress/LearnKNXAddress.ino
+++ KnxTpUart/examples/LearnKNXAddress/LearnKNXAddress.ino
@@ -1,3 +1,8 @@
+// File: LearnKNXAddress.ino  
+// Author: Daniel Kleine-Albers (Since 2012) 
+// Modified: Thorsten Gehrig (Since 2014)
+// Modified: Mag Gyver (Since 2016)
+
 #include <KnxTpUart.h>
 
 // Initialize the KNX TP-UART library on the Serial1 port of Arduino Mega
@@ -40,18 +45,22 @@ void serialEvent1() {
       Serial.println(member);   
 
       knx.setIndividualAddress(area, line, member);    
-   
+
       // Here the new address could be stored to EEPROM and be reloaded after restart of Arduino   
-    } else if (telegram->getCommand() == KNX_COMMAND_MASK_VERSION_READ) {
+    } 
+    else if (telegram->getCommand() == KNX_COMMAND_MASK_VERSION_READ) {
       // Request for mask version (version of bus interface
       knx.individualAnswerMaskVersion(telegram->getSourceArea(), telegram->getSourceLine(), telegram->getSourceMember());
-    } else if (telegram->getCommand() == KNX_COMMAND_INDIVIDUAL_ADDR_REQUEST && programmingMode) {
+    } 
+    else if (telegram->getCommand() == KNX_COMMAND_INDIVIDUAL_ADDR_REQUEST && programmingMode) {
       // Broadcast request for individual addresses of all devices in programming mode
       knx.individualAnswerAddress(); 
-    } else if (telegram->getFirstDataByte() == KNX_EXT_COMMAND_AUTH_REQUEST && programmingMode) {
+    } 
+    else if (telegram->getFirstDataByte() == KNX_EXT_COMMAND_AUTH_REQUEST && programmingMode) {
       // Authentication request to allow memory access
       knx.individualAnswerAuth(15, telegram->getSequenceNumber(), telegram->getSourceArea(), telegram->getSourceLine(), telegram->getSourceMember());
-    } else if (telegram->getCommand() == KNX_COMMAND_RESTART && programmingMode) {
+    } 
+    else if (telegram->getCommand() == KNX_COMMAND_RESTART && programmingMode) {
       // Restart the device -> end programming mode
       programmingMode = false;
       knx.setListenToBroadcasts(false);
