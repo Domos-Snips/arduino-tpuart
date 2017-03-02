a4786a15b9e0561ac4991c736ff799e00c8c20a2
Mag Gyver <foxfox@versanet.de>
2017-03-02 13:28:13 +0000

    Revision

--- KnxTpUart/examples/ReceiveKNXTelegrams/ReceiveKNXTelegrams.ino
+++ KnxTpUart/examples/ReceiveKNXTelegrams/ReceiveKNXTelegrams.ino
@@ -3,9 +3,11 @@
 // Modified: Thorsten Gehrig (Since 2014)
 // Modified: Mag Gyver (Since 2016)
 
+// Test constellation = ARDUINO MEGA <-> 5WG1 117-2AB12
+
 #include <KnxTpUart.h>
 
-// Initialize the KNX TP-UART library on the Serial1 port of Arduino Mega
+// Initialize the KNX TP-UART library on the Serial1 port of ARDUINO MEGA
 // and with KNX physical address 15.15.20
 KnxTpUart knx(&Serial1, "15.15.20");
 
