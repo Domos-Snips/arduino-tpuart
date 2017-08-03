ba4d698d06f234fc5fb9dfc59249d403a2d169fc
Mag Gyver <foxfox@versanet.de>
2017-08-03 21:14:40 +0000

    modified

--- KnxTpUart/examples/ReplyToKNXRead/ReplyToKNXRead.ino
+++ KnxTpUart/examples/ReplyToKNXRead/ReplyToKNXRead.ino
@@ -1,9 +1,17 @@
-// File: ReplyToKNXRead.ino   
-// Author: Daniel Kleine-Albers (Since 2012) 
-// Modified: Thorsten Gehrig (Since 2014)
-// Modified: Mag Gyver (Since 2016)
+/*
 
-// Test constellation = ARDUINO MEGA <-> 5WG1 117-2AB12
+   File: ReplyToKNXRead.ino
+
+   Author: Daniel Kleine-Albers (Since 2012)
+   Modified: Thorsten Gehrig (Since 2014)
+   Modified: Mag Gyver (Since 2016)
+
+   Last modfified: 03.08.2017
+   Reason: Clarity
+
+   Test constellation = ARDUINO MEGA <-> 5WG1 117-2AB12
+
+*/
 
 #include <KnxTpUart.h>
 
@@ -14,7 +22,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println("TP-UART Test");
 
-  Serial1.begin(19200, SERIAL_8E1);
+  Serial1.begin(19200, SERIAL_8E1); // Even parity
 
   Serial.print("UCSR1A: ");
   Serial.println(UCSR1A, BIN);
@@ -27,6 +35,7 @@ void setup() {
 
   knx.uartReset();
   // Define group address to react on
+  knx.addListenGroupAddress("3/0/0");
   knx.addListenGroupAddress("3/0/1");
   knx.addListenGroupAddress("3/0/2");
   knx.addListenGroupAddress("3/0/3");
@@ -34,7 +43,6 @@ void setup() {
   knx.addListenGroupAddress("3/0/5");
   knx.addListenGroupAddress("3/0/6");
   knx.addListenGroupAddress("3/0/7");
-  knx.addListenGroupAddress("3/0/8");
 }
 
 void loop() {
@@ -53,46 +61,48 @@ void serialEvent1() {
     // Is it a read request?
     if (telegram->getCommand() == KNX_COMMAND_READ) {
 
+      // Is the destination address equal to group address 3/0/0?
+      if (target == "3/0/0") {
+        knx.groupAnswerBool("3/0/0", true);
+        // Display: 1
+      }
       // Is the destination address equal to group address 3/0/1?
       if (target == "3/0/1") {
-        knx.groupAnswerBool("3/0/1", true);
-        // Display serial port : 1
+        knx.groupAnswer1ByteInt("3/0/1", 126);
+        // Display: 49%
       }
       // Is the destination address equal to group address 3/0/2?
       if (target == "3/0/2") {
-        knx.groupAnswer1ByteInt("3/0/2", 126);
-        // Display serial port : 49%
+        knx.groupAnswer2ByteInt("3/0/2", 1000);
+        // Display: 1000
       }
       // Is the destination address equal to group address 3/0/3?
       if (target == "3/0/3") {
-        knx.groupAnswer2ByteInt("3/0/3", 1000);
-        // Display serial port : 1000
+        knx.groupAnswer2ByteFloat("3/0/3", 25.28);
+        // Display: 25,28
       }
       // Is the destination address equal to group address 3/0/4?
       if (target == "3/0/4") {
-        knx.groupAnswer2ByteFloat("3/0/4", 25.28);
-        // Display serial port : 25,28
+        knx.groupAnswer3ByteTime("3/0/4", 7, 0, 0, 1);
+        // Display: Sonntag 00:00:01
       }
       // Is the destination address equal to group address 3/0/5?
       if (target == "3/0/5") {
-        knx.groupAnswer3ByteTime("3/0/5", 7, 0, 0, 1);
-        // Display serial port : Sonntag 00:00:01
+        knx.groupAnswer3ByteDate("3/0/5", 31, 1, 3);
+        // Display: 31.01.2003
       }
       // Is the destination address equal to group address 3/0/6?
       if (target == "3/0/6") {
-        knx.groupAnswer3ByteDate("3/0/6", 31, 1, 3);
-        // Display serial port : 31.01.2003
+        knx.groupAnswer4ByteFloat("3/0/6", -100);
+        // Display: -100
       }
       // Is the destination address equal to group address 3/0/7?
       if (target == "3/0/7") {
-        knx.groupAnswer4ByteFloat("3/0/7", -100);
-        // Display serial port : -100
-      }
-      // Is the destination address equal to group address 3/0/8?
-      if (target == "3/0/8") {
-        knx.groupAnswer14ByteText("3/0/8", "Hallo");
-        // Display serial port : "Hallo"
+        knx.groupAnswer14ByteText("3/0/7", "Hallo");
+        // Display: "Hallo"
       }
     }
   }
 }
+
+/* End of ReplyToKNXRead.ino */
